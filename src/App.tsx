853477317ca394bbcf50ca3a29f69ab5d68c72bf
kingshuann <kingshuann@gmail.com>
2026-04-03 02:54:19 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -398,7 +398,7 @@ const App = () => {
                   {/* Anatomical Reference Image */}
                   <div className="bg-white rounded-3xl overflow-hidden border border-gray-100 mb-6">
                     <img 
-                      src="./img/ear.jpg" 
+                      src="/img/ear.jpg" 
                       alt="Piercing Anatomy Reference" 
                       className="w-full h-auto object-contain"
                       referrerPolicy="no-referrer"
